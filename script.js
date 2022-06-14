c2d2c43408151136e3e93cc3abbfc07f682278f3
yuriy <59261753+testown@users.noreply.github.com>
2022-06-14 09:58:21 +0300

    home work 10

---
+++ script.js
@@ -0,0 +1,26 @@
+﻿function asking() {
+
+    let arrayInputs = [];
+    let arrayInputsCounter = 0;
+
+    do {
+        let input = prompt('Enter text:');
+        if (input === '' || input === null) break;
+        arrayInputs[arrayInputsCounter++] = input;
+    } while (true);
+
+    arrayInputs.forEach((item, index) => {
+        arrayInputs[index] = `${item[0].toUpperCase()}${item.slice(1)}`;
+    });
+
+    let arrayResult = arrayInputs.sort((first, second) => {
+        if (first.length > second.length) return 1;
+        if (first.length < second.length) return -1;
+        if (first.length === second.length) return 0;
+    });
+
+    return arrayResult;
+}
+
+console.log(asking());
+
